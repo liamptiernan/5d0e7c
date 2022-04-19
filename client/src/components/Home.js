3eda9f9e3bb1d7fe7afe4594c02de242a8d3e62c
Liam Tiernan <61879681+liamptiernan@users.noreply.github.com>
2022-04-19 12:37:13 -0400

    Message Appearance and Order Fix (#2)
    * Message Appearance and Order Fix
    
    Fixed bug causing messages to not appear when sent in existing or new conversations. This was caused by direct modification of state and an unawaited promise.
    
    Updated API to return messages in chronological order, with the last message appearing last.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -62,9 +62,9 @@ const Home = ({ user, logout }) => {
     });
   };
 
-  const postMessage = (body) => {
+  const postMessage = async (body) => {
     try {
-      const data = saveMessage(body);
+      const data = await saveMessage(body);
 
       if (!body.conversationId) {
         addNewConvo(body.recipientId, data.message);
@@ -80,16 +80,21 @@ const Home = ({ user, logout }) => {
 
   const addNewConvo = useCallback(
     (recipientId, message) => {
-      conversations.forEach((convo) => {
-        if (convo.otherUser.id === recipientId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
-          convo.id = message.conversationId;
-        }
+      setConversations((prev) => {
+        return prev.map((convo) => {
+          const convoCopy = { ...convo, messages: [ ...convo.messages ]}
+
+          if (convoCopy.otherUser.id === recipientId) {
+            convoCopy.messages.push(message);
+            convoCopy.latestMessageText = message.text;
+            convoCopy.id = message.conversationId;
+          }
+
+          return convoCopy;
+        });
       });
-      setConversations(conversations);
     },
-    [setConversations, conversations],
+    [setConversations],
   );
 
   const addMessageToConversation = useCallback(
@@ -106,15 +111,20 @@ const Home = ({ user, logout }) => {
         setConversations((prev) => [newConvo, ...prev]);
       }
 
-      conversations.forEach((convo) => {
-        if (convo.id === message.conversationId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
-        }
+      setConversations((prev) => {
+        return prev.map((convo) => {
+          const convoCopy = { ...convo, messages: [ ...convo.messages ]}
+
+          if (convoCopy.id === message.conversationId) {
+            convoCopy.messages.push(message);
+            convoCopy.latestMessageText = message.text;
+          }
+  
+          return convoCopy;
+        });
       });
-      setConversations(conversations);
     },
-    [setConversations, conversations],
+    [setConversations],
   );
 
   const setActiveChat = (username) => {
